8719ce47e87c43553314d05e4bcbcfd1a3978ab4
이도현 <60390155+dobot0101@users.noreply.github.com>
2022-08-29 23:42:27 +0900

    Time: 242 ms (51.95%), Space: 15.2 MB (48.56%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.py
+++ 724-find-pivot-index/724-find-pivot-index.py
@@ -9,11 +9,11 @@ class Solution:
             left_sum += num
 
         return -1
-        
+
+# 현재 인덱스에 따라 왼쪽 합 / 오른쪽 합을 구하고 비교하는 방법
+# Time Limit Exceeded로 submission을 통과하지 못함
 #         pivot_idx = -1
 #         for i in range(0, len(nums)):
-#             left_sum = 0
-#             right_sum = 0
 #             if i == 0:
 #                 left_sum = 0
 #                 right_sum = sum(nums[i+1: len(nums)])
@@ -23,15 +23,7 @@ class Solution:
 #             elif i == len(nums) - 1:
 #                 left_sum = sum(nums[0:len(nums)])
 #                 right_sum = 0
-            
-#             for j in range(0, len(nums)):
-#                 if j < i:
-#                     left_sum += nums[j]
-#                 elif j == i:
-#                     continue
-#                 elif j > i:
-#                     right_sum += nums[j]
-                    
+
 #             if left_sum == right_sum:
 #                 pivot_idx = i
 #                 break 
