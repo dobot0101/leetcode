ae6f1c453f7cc9d5710ddfe8504f2903fe369426
이도현 <60390155+dobot0101@users.noreply.github.com>
2022-09-13 23:12:47 +0900

    Time: 63 ms (13.37%), Space: 13.9 MB (75.74%) - LeetHub

--- 100-same-tree/100-same-tree.py
+++ 100-same-tree/100-same-tree.py
@@ -7,18 +7,47 @@
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
         
-        def checkSame(f, s):
-            if f == None and s == None:
-                return True
+        # recursive
+#         def checkSame(f, s):
+#             if f == None and s == None:
+#                 return True
             
-            if (f != None and s == None) or (f == None and s != None):
+#             if (f != None and s == None) or (f == None and s != None):
+#                 return False
+            
+#             if f.val != s.val:
+#                 return False
+            
+#             return checkSame(f.left, s.left) and checkSame(f.right, s.right)
+            
+#         return checkSame(p, q)
+    
+        # bfs
+        q1 = deque()
+        q2 = deque()
+        q1.append(p)
+        q2.append(q)
+        
+        while q1 and q2:
+            v1 = q1.popleft()
+            v2 = q2.popleft()
+            
+            if v1 == None and v2 == None:
+                continue
+            
+            if v1 == None or v2 == None:
                 return False
             
-            if f.val != s.val:
+            if v1.val != v2.val:
                 return False
             
-            return checkSame(f.left, s.left) and checkSame(f.right, s.right)
+            q1.append(v1.left)
+            q1.append(v1.right)
+            q2.append(v2.left)
+            q2.append(v2.right)
             
-        return checkSame(p, q)
+        return True
+            
+            
             
         
