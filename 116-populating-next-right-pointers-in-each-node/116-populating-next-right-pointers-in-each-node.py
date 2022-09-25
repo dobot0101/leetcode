d5abb5f35b75ff7a36c1cbd67b08113d3c6b7e16
Dohyun Lee <60390155+dobot0101@users.noreply.github.com>
2022-09-25 16:59:03 +0900

    Time: 109 ms (50.08%), Space: 15.5 MB (96.68%) - LeetHub

--- 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.py
+++ 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.py
@@ -20,33 +20,13 @@ class Solution:
             node = queue.popleft()
             if node.left and node.right:
                 node.left.next = node.right
-                queue.append(node.left)
-                queue.append(node.right)
                 
                 if node.next:
                     node.right.next = node.next.left
+                    
+                queue.append(node.left)
+                queue.append(node.right)
                 
         return root
-        
-#         while queue:
-#             queue_len = len(queue)
-#             tmp_arr = []
-#             for _ in range(queue_len):
-#                 node = queue.popleft()
-#                 tmp_arr.append(node)
-                
-#                 if node.left:
-#                     queue.append(node.left)
-                    
-#                 if node.right:
-#                     queue.append(node.right)
-        
-#             if tmp_arr:
-#                 tmp_arr.append(None)
-#                 result += tmp_arr
-#                 # result += list(map(str, tmp_arr))
-#                 print(result)
-        
-        return result
                 
         
