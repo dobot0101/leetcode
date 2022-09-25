a64686ff22452b41e553c8e84929e6d0148ccced
Dohyun Lee <60390155+dobot0101@users.noreply.github.com>
2022-09-25 21:45:53 +0900

    Time: 108 ms (17.89%), Space: 15.3 MB (92.43%) - LeetHub

---
+++ 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.py
@@ -0,0 +1,88 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        if not root:
+            return None
+        
+        q = deque()
+        q.append(root)
+        
+        while q:
+            node = q.popleft()
+            
+#             if node.left:
+#                 if node.right:
+#                     node.left.next = node.right
+#                 else:
+#                     next_node = node.next
+#                     while next_node:
+#                         if node.next.left:
+#                             node.left.next = node.next.left
+#                             break
+#                         elif node.next.right:
+#                             node.left.next = node.next.right
+#                             break
+                        
+#                         next_node = next_node.next
+            
+            if node.left and node.right:
+                node.left.next = node.right
+            elif node.left and node.right == None:
+                next_node = node.next
+                while next_node:
+                    if next_node.left:
+                        node.left.next = next_node.left
+                        break
+                    elif next_node.right:
+                        node.left.next = next_node.right
+                        break
+                    next_node = next_node.next
+                    
+            if node.right:
+                next_node = node.next
+                while next_node:
+                    if next_node.left:
+                        node.right.next = next_node.left
+                        break
+                    elif next_node.right:
+                        node.right.next = next_node.right
+                        break
+                    next_node = next_node.next
+                    
+                
+            
+            # if node.next:
+            #     if node.next.left:
+            #         if node.right:
+            #             node.right.next = node.next.left
+            #         elif node.left:
+            #             node.left.next = node.next.left
+            #     elif node.next.right:
+            #         if node.right:
+            #             node.right.next = node.next.right
+            #         elif node.left:
+            #             node.left.next = node.next.right
+                        
+            if node.left:
+                q.append(node.left)
+            
+            if node.right:
+                q.append(node.right)
+                
+                
+        return root
+                    
+                
+                    
+            
+                    
+                
